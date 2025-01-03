d14a7e9ca2671171421d0cbd5e9d245f25725121
Jon <jonkurtislee@yahoo.com>
2025-01-03 13:53:49 -0600

    Remove Gap

--- SysBot.Pokemon.Discord/Commands/Bots/TradeModule.cs
+++ SysBot.Pokemon.Discord/Commands/Bots/TradeModule.cs
@@ -1783,10 +1783,6 @@ public class TradeModule<T> : ModuleBase<SocketCommandContext> where T : PKM, ne
         public int TradeCount { get; set; }
     }
 
-
-
-
-
     [Command("medals")]
     [Alias("ml")]
     [Summary("Shows your current trade count and medal status")]
