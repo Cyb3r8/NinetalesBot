742cff863ac3e954cecead0bc11c8350d7b94116
Ninetale's Paradise <119709121+Cyb3r8@users.noreply.github.com>
2025-06-04 06:26:01 +0200

    misc

--- SysBot.Pokemon.WinForms/Main.cs
+++ SysBot.Pokemon.WinForms/Main.cs
@@ -1,8 +1,11 @@
 using PKHeX.Core;
 using SysBot.Base;
+using SysBot.Pokemon.Helpers;
+using SysBot.Pokemon.WinForms.Properties;
 using SysBot.Pokemon.Z3;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
